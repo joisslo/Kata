4a89d96ff12f522b0bcf0746544f8a68c955202b
Joseph Rudeseal <josephrudesealdev@gmail.com>
2017-12-16 17:52:21 -0500

    Fixing contributor name

--- ConsoleApp1/Kata.cs
+++ ConsoleApp1/Kata.cs
@@ -13,7 +13,8 @@ namespace CodeKata
         // Using List
         public static int[] ArrayDiff(int[] a, int[] b)
         {
-            // Your goal in this kata is to implement an difference function, which subtracts one list from another.
+            // Your goal in this kata is to implement an difference function, which subtracts one
+            // list from another.
 
             // It should remove all values from list a, which are present in list b.
 
@@ -48,7 +49,8 @@ namespace CodeKata
         // Resizing the array using Array.resize<>()
         public static int[] ArrayDiff2(int[] a, int[] b)
         {
-            // Your goal in this kata is to implement an difference function, which subtracts one list from another.
+            // Your goal in this kata is to implement an difference function, which subtracts one
+            // list from another.
 
             // It should remove all values from list a, which are present in list b.
 
@@ -83,7 +85,8 @@ namespace CodeKata
         // Resizing the array manually
         public static int[] ArrayDiff3(int[] a, int[] b)
         {
-            // Your goal in this kata is to implement an difference function, which subtracts one list from another.
+            // Your goal in this kata is to implement an difference function, which subtracts one
+            // list from another.
 
             // It should remove all values from list a, which are present in list b.
 
@@ -462,8 +465,7 @@ namespace CodeKata
 
         #region Tribonacci(double[] signature, int n)
 
-        // Fibonacci sequencer except summing previous 3 values instead of 2
-        // Manually
+        // Fibonacci sequencer except summing previous 3 values instead of 2 Manually
         public static double[] Tribonacci(double[] signature, int n)
         {
             if (n == 0)
@@ -530,7 +532,28 @@ namespace CodeKata
 
         #endregion CountSmileys(string[] smileys)
 
-        #region
-        #endregion
+        #region BouncingBall(double h, double bounce, double window)
+
+        //A child plays with a ball on the nth floor of a big building.The height of this floor is known.
+        //(float parameter "h" in meters.Condition 1) : h > 0)
+        //He lets out the ball.The ball bounces for example to two-thirds of its height.
+        //(float parameter "bounce". Condition 2) : 0 < bounce< 1)
+        //His mother looks out of a window that is 1.5 meters from the ground.
+        //(float parameters "window". Condition 3) : window<h).
+        //How many times will the mother see the ball either falling or bouncing in front of the window?
+        //If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+        //Note
+        //You will admit that the ball can only be seen if the height of the rebouncing
+        //ball is stricty greater than the window parameter.
+        //Example:
+        //h = 3, bounce = 0.66, window = 1.5, result is 3
+        //h = 3, bounce = 1, window = 1.5, result is -1 (Condition 2) not fulfilled).
+
+        public static int BouncingBall(double h, double bounce, double window)
+        {
+            return 0;
+        }
+
+        #endregion BouncingBall(double h, double bounce, double window)
     }
 }
